0b36775fe263574cf67107ee854ba40ef4a1be21
muj <99077742+muj-i@users.noreply.github.com>
2023-06-08 00:29:47 +0600

    ....

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+///import 'pages.dart'; //using stateful
+///import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -46,11 +48,13 @@ MyAletrtDialog(context) {
         ));
       });
 }
-//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+
 
 class Activity extends StatelessWidget {
   const Activity({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,20 +62,83 @@ class Activity extends StatelessWidget {
         title: Text('Resouces'),
         backgroundColor: Colors.pink,
       ),
-      body: Center(
-        child: ElevatedButton(
+
+///////////////////////////////////////
+/// Using a exter package named google_nav_bar as bottom navigation bar
+///import 'package:google_nav_bar/google_nav_bar.dart';
+///google_nav_bar: ^5.0.6
+/// int _currentIndex = 1; /// using statefull
+
+/*      bottomNavigationBar: Container(
+          color: const Color.fromARGB(255, 10, 29, 66).withAlpha(1000),
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Delete',
-              style: TextStyle(fontSize: 50),
-            ),
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
+              child: GNav(
+                  backgroundColor:
+                      const Color.fromARGB(255, 10, 29, 66).withAlpha(0),
+                  color: Colors.white,
+                  iconSize: 24,
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 9),
+                  activeColor: Colors.white,
+                  tabBackgroundColor:
+                      Color.fromARGB(255, 228, 231, 238).withOpacity(0.1),
+                  gap: 8,
+                  /////currentIndex: 1,
+                  tabs: [
+                    GButton(icon: Icons.home, text: "Page 1"),
+                    GButton(
+                        icon: Icons.business_center_outlined, text: "Page 2"),
+                    GButton(icon: Icons.people, text: "Page 3"),
+                  ],
+                  onTabChange: (int index) {
+                    //setState(() {
+                    // _currentIndex = index;
+                  }))),
+*/
+
+////////////////////////////////////
+
+//////////////////////////////////////////
+///using default bottom nav bar
+bottomNavigationBar: Container(
+  color: Colors.black,
+  child:   Padding(
+    padding: const EdgeInsets.all(16.0),
+    child:   BottomNavigationBar(
+        backgroundColor:Colors.amber,
+        iconSize: 24,
+       selectedItemColor: Colors.green,
+       currentIndex: 1,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Page 1"),
+          BottomNavigationBarItem(icon: Icon(Icons.sailing), label: "Page 2"),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: "Page 3"),
+        ],
+        onTap: (int index) {
+          //setState(() {
+          // _currentIndex = index;
+        }),
+  ),
+),
+/////////////////////////////////////////////
+
+
+
+    body:
+    Center(
+      child: ElevatedButton(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            'Delete',
+            style: TextStyle(fontSize: 50),
           ),
-          onPressed: () {
-            MyAletrtDialog(context);
-          },
         ),
+        onPressed: () {
+          MyAletrtDialog(context);
+        },
       ),
+    ),
     );
   }
 }
